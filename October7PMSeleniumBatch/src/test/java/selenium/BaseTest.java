67a95f815f4d99080c46986a94580a07814846e2
ravilella1234 <masters.selenium@gmail.com>
2022-10-28 19:44:33 +0530

    log4j

--- October7PMSeleniumBatch/src/test/java/selenium/BaseTest.java
+++ October7PMSeleniumBatch/src/test/java/selenium/BaseTest.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Properties;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -53,6 +54,9 @@ public class BaseTest
 		fis = new FileInputStream(projectPath+"or.properties");
 		orProp = new Properties();
 		orProp.load(fis);
+		
+		fis = new FileInputStream(projectPath+"log4jconfig.properties");
+		PropertyConfigurator.configure(fis);
 	}
 	
 	public static void launch(String browser)
