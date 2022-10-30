2fb9f337170fc67c33c8aecc94d7243fab71b6be
ravilella1234 <masters.selenium@gmail.com>
2022-10-30 07:41:46 +0530

    added first line

--- October7PMSeleniumBatch/src/test/java/selenium/DummyClass.java
+++ October7PMSeleniumBatch/src/test/java/selenium/DummyClass.java
@@ -2,8 +2,9 @@ package selenium;
 
 public class DummyClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		System.out.println("iam first");
 
 	}
 
